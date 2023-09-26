576202674802508d57cf74679277ee9393c5d6e3
Eduardo Menegaz Pretto <eduardo.pretto@edu.pucrs.br>
2023-09-26 14:40:57 -0300

    finished moving joystick

--- ArcherRun/GameScene.swift
+++ ArcherRun/GameScene.swift
@@ -34,6 +34,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var joystick: Joystick = Joystick()
     
+    var bowJoystick: Joystick = Joystick()
+    
     let cam = SKCameraNode()
     
 //    let frames:[SKTexture] = createTexture("Character")
@@ -76,16 +78,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 //        rightButton.position = CGPoint(x: -240, y: -130)
 //        cam.addChild(rightButton)
        
-        jumpButton.position = CGPoint(x: 340, y: -130)
-        cam.addChild(jumpButton)
-     
-        fireButton.position = CGPoint(x: 240, y: -130)
-        cam.addChild(fireButton)
+//        jumpButton.position = CGPoint(x: 340, y: -130)
+//        cam.addChild(jumpButton)
+//
+//        fireButton.position = CGPoint(x: 240, y: -130)
+//        cam.addChild(fireButton)
         
-        //joystick
+        //movement joystick
         joystick.position = CGPoint(x: -290, y: -110)
         cam.addChild(joystick)
         
+        //bow joystick
+        bowJoystick.position = CGPoint(x: 290, y: -110)
+        cam.addChild(bowJoystick)
         
         
         let spawnAction = SKAction.run { [weak self] in
@@ -137,41 +142,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
            jumpButton.position = CGPoint(x: 340, y: -130)
            fireButton.position = CGPoint(x: 240, y: -130)
 
-        if isLeftButtonPressed {
-            moveCharacterLeft()
-        } else if isRightButtonPressed {
-            moveCharacterRight()
-        }
-        
+//        if isLeftButtonPressed {
+//            moveCharacterLeft()
+//        } else if isRightButtonPressed {
+//            moveCharacterRight()
+//        }
+//
         if character.physicsBody?.allContactedBodies().contains(ground.physicsBody!) ?? false {
             isCharacterJumping = false
         }
         
         //joystick
-        
-        if joystick.velocity.x != 0{
-            character.position = CGPointMake(character.position.x + 0.1 * joystick.velocity.x, character.position.y)
-            if joystick.velocity.x > 0 {
-                let frames:[SKTexture] = createTexture("Character")
-                character.run(SKAction.repeat(SKAction.animate(with: frames,
-                                                                       timePerFrame: TimeInterval(0.1),
-                                                                       resize: false, restore: false), count: 1))
-                
-                isCharacterFacingRight = true
-                character.xScale = 1.0
-                print (joystick.velocity.y)
-            } else {
-                let frames:[SKTexture] = createTexture("Character")
-                character.run(SKAction.repeat(SKAction.animate(with: frames,
-                                                                       timePerFrame: TimeInterval(0.1),
-                                                                       resize: false, restore: false), count: 1))
-                character.xScale = -1.0
-                isCharacterFacingRight = false
-            }
-        }
-        if joystick.velocity.y > 50 && !isCharacterJumping{
-            jumpCharacter()
-        }
+        joystickActions()
     }
     
     func createTexture(_ name: String) -> [SKTexture] {
@@ -301,6 +283,32 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             if let enemyNode = contact.bodyB.node {
                 enemyNode.removeFromParent()
             }
+        }
+    }
+    
+    func joystickActions() {
+        if joystick.velocity.x != 0{
+            character.position = CGPointMake(character.position.x + 0.1 * joystick.velocity.x, character.position.y)
+            if joystick.velocity.x > 0 {
+                let frames:[SKTexture] = createTexture("Character")
+                character.run(SKAction.repeat(SKAction.animate(with: frames,
+                                                                       timePerFrame: TimeInterval(0.1),
+                                                                       resize: false, restore: false), count: 1))
+                
+                isCharacterFacingRight = true
+                character.xScale = 1.0
+                print (joystick.velocity.y)
+            } else {
+                let frames:[SKTexture] = createTexture("Character")
+                character.run(SKAction.repeat(SKAction.animate(with: frames,
+                                                                       timePerFrame: TimeInterval(0.1),
+                                                                       resize: false, restore: false), count: 1))
+                character.xScale = -1.0
+                isCharacterFacingRight = false
+            }
+        }
+        if joystick.velocity.y > 50 && !isCharacterJumping{
+            jumpCharacter()
         }
     }
 
